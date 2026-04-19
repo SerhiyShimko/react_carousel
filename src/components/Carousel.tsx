9083146e2f7a7363dd7e0f6767b3ba9d82574e71
Serhii Shimko <shimkosergij2010@gmail.com>
2026-04-19 16:16:34 +0300

    completed the task

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step = 3,
+  frameSize = 3,
+  itemWidth = 130,
+  animationDuration = 1000,
+  infinite = false,
+}) => {
+  const [startImage, changeStartImage] = useState(0);
+  const height = frameSize * itemWidth;
+  const imgs = images;
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel__window"
+        style={{
+          height: height,
+          overflow: 'hidden',
+        }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            transition: `transform ${animationDuration}ms ease`,
+            transform: `translateY(-${startImage * itemWidth}px)`,
+          }}
+        >
+          {imgs.map((img, i) => {
+            return (
+              <li
+                key={i}
+                style={{ width: itemWidth + 'px', height: itemWidth + 'px' }}
+              >
+                <img
+                  width={itemWidth}
+                  src={img}
+                  alt="2"
+                  style={{ width: itemWidth + 'px' }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <button
+        className="Carousel__button Carousel__button--prev"
+        type="button"
+        onClick={() => {
+          if (startImage - step >= 0) {
+            changeStartImage(prev => prev - step);
+          }
+        }}
+      >
+        ← Prev
+      </button>
+      <button
+        data-cy="next"
+        className="Carousel__button Carousel__button--next"
+        type="button"
+        onClick={() => {
+          if (infinite) {
+            if (!imgs[startImage + step]) {
+              changeStartImage(0);
+            }
+          }
+
+          if (startImage + step <= imgs.length) {
+            changeStartImage(prev => prev + step);
+          }
+        }}
+      >
+        Next →
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
